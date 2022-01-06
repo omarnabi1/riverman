9ef2f4a1db829359d6af457c81283e36561cd98e
omarnabi1 <onabi20@gmail.com>
2022-01-06 11:19:51 -0800

    price currency change

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import React from 'react';
 import './App.css';
-import data from "./data.json"
-import Products from "./components/Products"
 //feature 1
-class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      products: data.products,
-      size: "",
-      sort: "",
-    };
-  }
-
-  render() {
-    return (
-      <div className="grid-container">
-        <header>
-          <a href="/">Riverman</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main"> 
-              <Products products={this.state.products}></Products>
-             </div>
-            <div className="sidebar"> Cart Items </div>
-          </div>
-        </main>
-        <footer>All Rights Reserved</footer>
-      </div>
-    );
-  }
-  
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Riverman</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>All Rights Reserved.</footer>
+    </div>
+  );
 }
+
 export default App;
